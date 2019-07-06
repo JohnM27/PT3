2eca5dabb34a8b85797af330265ce3dd2ea4e47e
Mathéo <matheo.faivre@sfr.fr>
2019-07-06 17:05:20 +0200

    Add files via upload

--- view/HudSLPanel.java
+++ view/HudSLPanel.java
@@ -1,7 +1,9 @@
 package view;
 
 import java.awt.Dimension;
-import java.awt.GridLayout;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
 
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -11,17 +13,29 @@ public class HudSLPanel extends JPanel {
 	private JLabel pierre, bois, nourriture;
 	private int nbPierre = 0, nbBois = 0, nbNourriture = 0;
 	
+	GridBagConstraints c = new GridBagConstraints();
+	
 	public HudSLPanel() {
-		super(new GridLayout(0,1));
+		super(new GridBagLayout());
 		
-		pierre = new JLabel("Pierre: " + nbPierre);
-		bois = new JLabel("Bois: " + nbBois);
 		nourriture = new JLabel("Nourriture: " + nbNourriture);
 		
 		this.setPreferredSize(new Dimension(390, 240));
+		c.insets = new Insets(10, 10, 10, 10);
 		
-		add(pierre);
-		add(bois);
-		add(nourriture);
+		pierre = new JLabel("Pierre: " + nbPierre);
+		c.gridx = 0;
+		c.gridy = 0;
+		add(pierre, c);
+		
+		bois = new JLabel("Bois: " + nbBois);
+		c.gridx = 0;
+		c.gridy = 1;
+		add(bois, c);
+		
+		nourriture = new JLabel("Nourriture: " + nbNourriture);
+		c.gridx = 0;
+		c.gridy = 2;
+		add(nourriture, c);
 	}
 }
