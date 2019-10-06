e1b472f8a3cae69b3ce3d8a5f2a172a7465e2007
JohnM27 <jean.marc.diot.mail@gmail.com>
2019-10-06 02:40:37 +0200

    Test Commit Eclipse

--- model/MenuListener.java
+++
@@ -1,8 +0,0 @@
-package model;
-
-import java.util.EventListener;
-
-public interface MenuListener extends EventListener {
-
-	public void Menu(MenuEvent event);
-}
--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,6 @@ public class Main {
 	public static void main(String[] args) {
 		Model model = new Model();
 		Controller controller = new Controller(model);
-		controller.displayView();
+		//controller.displayView();
 	}
 }
